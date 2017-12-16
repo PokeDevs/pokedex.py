894dd2c3ea091c07beb77680409de57a4ec950b3
Sankarsan Kampa <me@sankarsankampa.com>
2017-12-16 23:29:18 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from codecs import open
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
+with open('requirements.txt', 'r', 'utf-8') as f:
+    requirements = f.readlines()
+
+setup(
+    name='pokedex.py',
+    version='1.0.0',
+    description='A Python library for the Pokedex API.',
+    long_description=readme,
+    url='https://github.com/PokeDevs/pokedex.py',
+    author='k3rn31p4nic',
+    license='GPL-3.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    keywords='pokemon pokedex api database pokeapi pokedb wrapper library',
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+)
